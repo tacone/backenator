2aa8b0931a5079a1e2086226a722361f01c7a9ee
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2013-06-07 17:22:13 +0000

    updated delete in model

--- src/EllipseSynergie/Backenator.php
+++ src/EllipseSynergie/Backenator.php
@@ -164,6 +164,32 @@ abstract class Backenator extends Eloquent {
 	}
 	
 	/**
+	 * Delete the model from the database.
+	 *
+	 * @return bool|null
+	 */
+	public function delete()
+	{
+		if ($this->fireModelEvent('deleting') === false) return false;
+	
+		// Here, we'll touch the owning models, verifying these timestamps get updated
+		// for the models. This will allow any caching to get broken on the parents
+		// by the timestamp. Then we will go ahead and delete the model instance.
+		$this->touchOwners();
+	
+		$this->performDeleteOnModel();
+	
+		$this->exists = false;
+	
+		// Once the model has been deleted, we will fire off the deleted event so that
+		// the developers may hook into post-delete operations. We will then return
+		// a boolean true as the delete is presumably successful on the database.
+		$this->fireModelEvent('deleted', false);
+	
+		return true;
+	}
+	
+	/**
 	 * Add query string parameter to the request
 	 *
 	 * @param string $field
